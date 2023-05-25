e24cad5b49bad0fddf832a4cced0ea11d72c4069
MuhammadEzraMaulana <ezra.10120098@mahasiswa.unikom.ac.id>
2023-05-25 17:34:30 +0700

    Tugasbesar

---
+++ admin/home.php
@@ -0,0 +1,5 @@
+<h2>Selamat datang Muhammad ezra</h2>
+
+<!-- <pre> -->
+  <?php // print_r($_SESSION); ?>
+<!-- </pre> -->
---
+++ admin/kategori.php
@@ -0,0 +1,33 @@
+<?php
+$semuadata = [];
+$ambil = $koneksi->query("SELECT * FROM kategori");
+while($tiap = $ambil->fetch_assoc()){
+  $semuadata[] = $tiap;
+}
+
+// echo "<pre>";
+// print_r($semuadata);
+// echo "</pre>";
+
+?>
+
+
+<h3>Data Kategori</h3>
+<br>
+
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th>No</th>
+      <th>Kategori</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($semuadata as $key => $value): ?>
+    <tr>
+      <td><?= $key+1; ?>.</td>
+      <td><?= $value['nama_kategori']; ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
